03fb136be99f37e6b077f02ecc109ba331d2a758
veres7 <veres7@users.noreply.github.com>
2020-12-09 19:06:04 +0100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import "./App.css";
+import Login from "./components/Login/login";
+function App() {
+  return (
+    <div className="App">
+      <CssBaseline />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
