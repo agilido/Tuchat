f83c56da980dad5d990ac064a8782ce113553bf3
agilido <75748053+agilido@users.noreply.github.com>
2020-12-16 16:06:43 +0100

    Login layout fixing, checkpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import "./App.css";
-import Login from "./components/Login/login";
+import Login from "./components/Access/login";
 function App() {
   return (
     <div className="App">
