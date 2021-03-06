39f404ce3d7dff80a278d2e3cd33ead42786be41
agilido <75748053+agilido@users.noreply.github.com>
2021-03-06 20:07:03 +0100

    chat model update, prettier

--- server/models/Chat.js
+++ server/models/Chat.js
@@ -1,19 +1,35 @@
-const mongoose = require("mongoose"); 
+const mongoose = require("mongoose");
 
 const ChatSchema = new mongoose.Schema({
-    chatId: String,
-    chatName: String,
-    messagesByDate: [{
-        date:String,
-        messages:[{
-            Id:String,
-            message: String,
-            time: Number,
-            readBy: [{
-                _id: false
-            }]
-        }]
-    }]
+  chatId: String,
+  chatName: String,
+  members: [
+    {
+      _id: false,
+      userId: String,
+      username: String,
+    },
+  ],
+  messagesByDate: [
+    {
+      date: String,
+      messages: [
+        {
+          _id: false,
+          Id: String,
+          message: String,
+          time: Number,
+          readBy: [
+            {
+              _id: false,
+              userId: String,
+              username: String,
+            },
+          ],
+        },
+      ],
+    },
+  ],
 });
 
 module.exports = mongoose.model("GroupChat", ChatSchema);
