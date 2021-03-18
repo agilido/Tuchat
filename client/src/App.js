c94a6466b80a74d30aa6ced6173b329e712d7010
agilido <75748053+agilido@users.noreply.github.com>
2021-03-18 17:18:19 +0100

    Add routes to reset password pages

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,24 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import "./App.css";
 import Landing from "./components/Access/Landing";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import PrivateRoute from './components/PrivateRoute';
+import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/Access/Login";
 import Register from "./components/Access/Register";
+import fpassword from "./components/Access/fpassword";
+import resetpassword from "./components/Access/resetpassword";
 import Home from "./components/Home/Home";
 function App() {
   return (
     <Router>
       <div className="App">
         <CssBaseline />
-        <PrivateRoute exact path="/" component={Home}/>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
+        <Switch>
+          <PrivateRoute exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/fpassword" component={fpassword} />
+          <Route exact path="/resetpassword" component={resetpassword} />
+        </Switch>
       </div>
     </Router>
   );
