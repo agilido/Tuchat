342bd4464815c9d6afafd91f62f46a464ccc8331
agilido <75748053+agilido@users.noreply.github.com>
2021-03-23 14:56:56 +0100

    Add Home auth check

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,14 +1,49 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 export const Home = () => {
-    const style = {
-        color: "white"
+  const style = {
+    color: "white",
+  };
+  const [error, setError] = useState("");
+  const [PrivateData, setPrivateData] = useState("");
+
+  let history = useHistory();
+  useEffect(() => {
+    if (localStorage.getItem("authToken")) {
+      history.push("/");
     }
-    return (
-        <div>
-            <h1 style={style}>YO IM CHAT MAIN SCREEN</h1>
-        </div>
-    )
-}
+    const fetchPrivateData = async () => {
+      const config = {
+        header: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${localStorage.getItem("authToken")}`,
+        },
+      };
+      try {
+        const { data } = await axios.get("/api/private", config);
+        setPrivateData(data.data);
+      } catch (error) {
+        localStorage.removeItem("authToken");
+        setError("Not authorized");
+      }
+      fetchPrivateData();
+    };
+  }, [history]);
+
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    history.push("/login");
+  };
+
+  return (
+    <div>
+      {error && <span>{error}</span>}
+      <h1 style={style}>YO IM CHAT HOME SCREEN</h1>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+};
 
 export default Home;
