7d591173eb4204c68cc278b590e13658d77e593a
veres7 <veres7@users.noreply.github.com>
2021-04-02 17:35:10 +0200

    Change style imports

--- client/src/components/Access/login.js
+++ client/src/components/Access/login.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./authPage.css";
+import "./styles.css";
 import { TextField, Button, Typography } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import AccountCircle from "@material-ui/icons/AccountCircle";
