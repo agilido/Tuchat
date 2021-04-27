6f7ad077766785adfe9debf4345b6474e21168ce
veres7 <veres7@users.noreply.github.com>
2021-04-27 12:44:07 +0200

    Add channel context, title change on open

--- client/src/context/socket.js
+++ client/src/context/socket.js
@@ -1,6 +1,6 @@
-import React from "react";
+import { createContext } from "react";
 import { io } from "socket.io-client";
 
 export const socket = io();
 
-export const SocketContext = React.createContext();
+export const SocketContext = createContext();
